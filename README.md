06ca9ebf3a8f95c1b2573f5c69a68ecf8b03cfd7
ttrreevvoorr <trevormaustin@pm.me>
2023-04-28 22:10:20 -0700

    gitignore, and allow for tracks

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 Download songs from YouTube based on tracks found in a Spotify URL 
 
+## Todo:
+
+- Allow config to change default download DIR
+- Allow config to allow users to alter default ytsr query
+
 ![image](https://user-images.githubusercontent.com/53444861/235282442-68284d51-8c76-43f4-92b7-a946cbe46402.png)
--- src/utils.js
+++ src/utils.js
@@ -16,46 +16,18 @@ const slash = !isWin ? "/" : "\\";
 
 module.exports = {
   /**
-   * Returns a YouTube URL to play music with the "play" property of your preferred music npm.
-   * @param {String} url Spotify URL
-   * @returns YouTube Info
-   */
-  trackGet: async (url) => {
-    await validateURL(url);
-    let data = await getData(url);
-
-    if (data.type === "track") {
-      let query = `${data.name} ${data.artists.map((x) => x.name).join(" ")}`;
-      let search = await ytsr(query, { limit: 1 });
-
-      if (!search) {
-        console.log("\x1b[31m%s\x1b[0m", "Found no results from YouTube");
-        process.exit(0);
-      }
-      return {
-        url: search[0].url,
-        info: search,
-      };
-    } else {
-      console.log("\x1b[31m%s\x1b[0m", "The URL type is invalid!");
-      process.exit(0);
-    }
-  },
-
-  /**
    * Returns an Array of YouTube music URL's to play a playlist with your preferred music npm.
    * @param {String} url Spotify URL
    * @returns Array with YouTube URL's
    */
   playListGet: async (url) => {
-    await validateURL(url);
+    await _validateURL(url);
     let data = await getData(url);
     let tracks = await getTracks(url);
     let details = await getDetails(url);
 
-    if (data.type !== "playlist" && data.type !== "album") {
-      console.log("\x1b[31m%s\x1b[0m", "The URL is invalid!");
-      process.exit(0);
+    if (!supportedTypes.includes(data.type)) {
+      console.log("\x1b[31m%s\x1b[0m", "The URL is invalid! Please restart");
     }
 
     try {
@@ -87,10 +59,9 @@ module.exports = {
     } catch (e) {
       console.log(
         "\x1b[31m%s\x1b[0m",
-        "An error occured while trying to get information from the playlist:\n" +
+        "An error occured while trying to get information from the playlist. Please restart:\n" +
           e
       );
-      process.exit(0);
     }
   },
 
@@ -112,11 +83,16 @@ module.exports = {
   },
 };
 
-// Validate the Spotify URL
-async function validateURL(url) {
+/**
+ * Validate the Spotify URL
+ * @param {string} url
+ */
+async function _validateURL(url) {
   if (!url || typeof url !== "string") {
-    console.log("\x1b[31m%s\x1b[0m", "You did not specify the URL of Spotify!");
-    process.exit(0);
+    console.log(
+      "\x1b[31m%s\x1b[0m",
+      "You did not specify the URL of Spotify! Please restart"
+    );
   }
 
   let regexp =
@@ -129,19 +105,16 @@ async function validateURL(url) {
       if (!supportedTypes.includes(parsedURL.type)) {
         console.log(
           "\x1b[31m%s\x1b[0m",
-          "The specified URL is not of a valid type. Valid types: " +
+          "The specified URL is not of a valid type. Please restart. Valid types: " +
             supportedTypes.join(", ")
         );
-        process.exit(0);
       }
       return true;
     } catch (e) {
       console.log("\x1b[31m%s\x1b[0m", e);
-      process.exit(0);
     }
   } else {
-    console.log("\x1b[31m%s\x1b[0m", "The link is invalid!");
-    process.exit(0);
+    console.log("\x1b[31m%s\x1b[0m", "The link is invalid! Please restart");
   }
 }
 
@@ -161,6 +134,6 @@ async function downloadSong(song, outputDir, fileName) {
   audio.pipe(fs.createWriteStream(filePath));
 
   audio.on("end", () => {
-    console.log("\x1b[32m%s\x1b[0m", `Downloaded: `, filePath);
+    console.log("\x1b[32m%s\x1b[0m", `Downloaded: `, path.resolve(filePath));
   });
 }
